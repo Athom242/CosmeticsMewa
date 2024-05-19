5da3b671ca9fa30691dc601b98016fe6d9de37bf
Moussavou Anthony <moussavouanthony@gmail.com>
2024-05-19 03:57:17 +0200

    Insertion des differents composant Page Home  enter the commit message for your changes. Lines starting

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,56 @@
 import React from "react";
 
 
-function chooseProductSelling(props){
+function ProductItem(props){
+    const price=Math.ceil(Math.random()*10);
+    const priceLast=Math.ceil(Math.random()*10);
     return(
-        <div className="sectionChoosePoductSelling">
-            <div className="containt">
-                <div className="containt">
-                    
+        <div className="productItem">
+            <div className="productItemContainer">
+                <div className="imgProduct">
+                    <div className="state">space</div>
                 </div>
+
+                <div className="bottomProductDescr">
+                    <div className="headerProduct">
+                        <div className="title">
+                            <a href="#">Lorem ipsum dolor sit amet.</a>
+                        </div>
+
+                        <div className="shortDescr">
+                            <a href="#">Lorem, ipsum.</a>
+                        </div>
+                    </div>
+
+                    <div className="price">
+                        <div className="priceLast">
+                            <span className="devise">$</span><span className="priceProduct">{priceLast}</span>
+                        </div>
+
+                        <div className="priceCurrent">
+                            <span className="devise">$</span><span className="priceProduct">{price}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+function ChooseProductSelling(props){
+    return(
+        <div className="sectionChooseProductSelling">
+            <div className="containt">
+                <div className="header">
+                    <h2 className="title">Find Your Beauty Match</h2>
+                    <h4 className="shortDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nisi.</h4>
+                </div>
+                <div className="productContainer">
+                    {[0,0,0,0,0,0,0,0,0,0,0,0].map(index=>{
+                        return(<ProductItem/>)
+                    })}
+                </div>
+
             </div>
         </div>
     )
@@ -29,6 +72,9 @@ function SectionTestimonial(props){
     return(
         <div className="sectionTestimonial">
             <div className="containt">
+                <div className="header">
+                    <h2 className="title">Temoignage</h2>
+                </div>
                 <div className="testimonialListInner">
                     {[0,0,0].map(index=>{
                         return(<TesimonalItem/>)
@@ -326,6 +372,7 @@ export default function Home(props){
                 <Header/>
                 <SectionPresProductList/>
                 <SectionTestimonial/>
+                <ChooseProductSelling/>
                 <SectionFooterContaint/>
                 <Footer/>
             </div>
